ce1d7a2d41041d35ae85801f6123044a7b038af3
ayush <ashuchauhan840@gmail.com>
2024-06-15 13:00:05 +0530

    db connection

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,7 +1,12 @@
-import express from "express";
 
+import express from "express";
+import routes from "./routes/index.js";
 //create express app
 const app = express();
+
+
+app.use("/api/v1", routes);
+
 app.get('/', (req,res)=>{
   res.send("hello from server")
 })
--- backend/src/index.js
+++ backend/src/index.js
@@ -1,11 +1,21 @@
 import app from "./app.js";
 import dotenv from "dotenv";
-
+import mongoose from "mongoose"
 //dotenv config
 dotenv.config();
 
 //env variables
 const PORT = process.env.PORT || 8000;
+const DATABASE_URL = process.env.DATABASE_URL;
+
+mongoose.connection.on("error", (err)=>{
+    console.log(`Mongodb connection error ${err}`)
+   
+})
+
+mongoose.connect(DATABASE_URL).then(
+    ()=>console.log("connected to mongodb")
+)
 
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}...`)
